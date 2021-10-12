e6596cb84eb674a09831613ca4882e8869153d51
Manbir A <manbir1125@gmail.com>
2021-10-12 19:27:48 -0400

    format changes

--- database.h
+++ database.h
@@ -10,22 +10,22 @@
 #define FILE_EXT "_string.kv"
 
 class database {
-    private:
-        std::string name;
-        std::string dir;
-        std::string base_dir;
-    public:
-        explicit database(std::string name);
-        database(std::string name, std::string full_path);
-        int delete_db();
-        static database *load_db(std::string name);
-        std::string get_name();
-        int set_name(std::string name);
-        std::string get_dir();
-        int set_dir(std::string dir);
-        std::string create_dir(std::string base_dir);
-        int set_key_value(std::string key, std::string value);
-        std::string get_key_value(std::string key);
+private:
+    std::string name;
+    std::string dir;
+    std::string base_dir;
+public:
+    explicit database(std::string name);
+    database(std::string name, std::string full_path);
+    int delete_db();
+    static database *load_db(std::string name);
+    std::string get_name();
+    int set_name(std::string name);
+    std::string get_dir();
+    int set_dir(std::string dir);
+    std::string create_dir(std::string base_dir);
+    int set_key_value(std::string key, std::string value);
+    std::string get_key_value(std::string key);
 };
 
 
